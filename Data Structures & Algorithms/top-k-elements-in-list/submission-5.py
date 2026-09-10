1c137cbc663f1726a6efe343bd52bb454ae939af
tanzilahmed0 <148499414+tanzilahmed0@users.noreply.github.com>
2026-09-10 14:47:16 -0700

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.py
@@ -0,0 +1,23 @@
+import heapq 
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        # the word frequency signals to me the use of some type of hashmap
+        # and then we can iterate through the hashmap and add the frequency and the 
+        # integer to a heap 
+        # then we can return the 2 most frequent items from the heap 
+        freqs = Counter(nums)
+        heap = []
+
+        for num, freq in freqs.items(): 
+            heapq.heappush(heap, (-freq, num))
+        
+        result = []
+        for i in range(k):
+            freq, value = heapq.heappop(heap)
+            result.append(value)
+
+        return result
+
+
