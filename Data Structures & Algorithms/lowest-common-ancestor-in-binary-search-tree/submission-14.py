541e198224f1e4859602981f21361c392e44e2eb
tanzilahmed0 <148499414+tanzilahmed0@users.noreply.github.com>
2026-09-14 15:27:23 -0700

    Add: lowest-common-ancestor-in-binary-search-tree - submission-14

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-14.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        # A node can be a descendant of itself 
+        # This is a binary search tree, meaning all the nodes on the left are smaller 
+        # and all the nodes on the right are bigger 
+        # So what we can check if the current node is in between p and q, if it is, 
+        # then that's the LCA 
+        # if the node is greater than them both, the LCA is on the left side 
+        # if it's less than them both, it's on the right side 
+        # At each node, what am i returning? what am i carrying up? 
+        # I don't need to use a helper function 
+        # It can be itself, so we need to include the root
+
+        if not root:
+            return None
+
+        elif p.val > root.val and q.val > root.val: 
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif p.val < root.val and q.val < root.val: 
+            return self.lowestCommonAncestor(root.left, p, q)
+
+        return root 
+
+        # Time Complexity: O(h) because at each node you only go one direction, so it's equal to the 
+        # height of the tree 
+        # Space Complexity: O(h) because of recursive call stack
