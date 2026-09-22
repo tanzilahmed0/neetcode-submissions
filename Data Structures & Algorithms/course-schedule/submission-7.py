3b17560e0d867d4c83ef0900d4295b071fa1e6b8
tanzilahmed0 <148499414+tanzilahmed0@users.noreply.github.com>
2026-09-21 17:35:35 -0700

    Add: course-schedule - submission-7

---
+++ Data Structures & Algorithms/course-schedule/submission-7.py
@@ -0,0 +1,43 @@
+from collections import defaultdict
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        # [0,1] [1, 3], [3, 4], [5, 6]
+        # 0 -> 1 - > 3 -> 4 -> 1 
+        # 5 -> 6
+        # Essentially if there is a cycle, we can't finish all courses, 
+        # So we can go through adjacency list and recursively go through each connected edge 
+        # we can use a visited set to track if we've run into the same course, which means it's not possible as the 
+        # courses are prereqs of each other
+        # [0: 1]
+        # [1: 3]
+
+        adjlist = defaultdict(list)
+        visited = set()
+
+        for a, b in prerequisites: 
+            adjlist[a].append(b)
+        
+        print(adjlist)
+        
+        def dfs(course, visited): 
+            if course in visited: 
+                return False
+            if adjlist[course] == []:
+                return True
+
+            visited.add(course) 
+            for prereq in adjlist[course]: 
+                if not dfs(prereq, visited): 
+                    return False
+            visited.remove(course)
+            adjlist[course] = []
+            return True
+        
+        for course in range(numCourses): 
+            if not dfs(course, visited): 
+                return False 
+        return True
+
+
+            
